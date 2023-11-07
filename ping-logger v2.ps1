c057f17efada7b2668789a8bba70b49e97e37d97
Gerardo <56288124+Dynecs@users.noreply.github.com>
2023-11-07 16:50:53 -0700

    Update ping-logger v2.ps1

--- ping-logger v2.ps1
+++ ping-logger v2.ps1
@@ -30,7 +30,7 @@ hostnmame,X.X.X.X,YYYY/MM/MM,HH:MM:SS,ms,username
 
 # File name variables:
 $hostname = $env:computername 
-$PACS = Get-Content "C:\Users\gguaracha\Documents\Scripts\F5LB.txt"
+$PACS = Get-Content "C:\Users\$USER\Documents\Scripts\F5LB.txt"
 
 # Time stamp format for the file name:
 $YEARSTART = get-date -format yyyy-MM-dd;
@@ -43,7 +43,7 @@ $logname = $DefaulLogName
 
 
 # CSV file setup:
-add-content C:\Users\gguaracha\Documents\Scripts\$logname "Rad WS,PACS,Latency,Date,Time,User";
+add-content C:\Users\$USER\Documents\Scripts\$logname "Rad WS,PACS,Latency,Date,Time,User";
 
 
 # Start time assumes successful ping to calculate seconds
@@ -72,7 +72,7 @@ While($true)
                 # Log failure to file.
                 $YEAR = get-date -format yyyy-MM-dd;
                 $TIME = get-date -format HH:mm:ss
-                add-content C:\Users\gguaracha\Documents\Scripts\$logname "$hostname,$PACS,>50,$YEAR,$TIME,$env:USERNAME";
+                add-content C:\Users\$USER\Documents\Scripts\$logname "$hostname,$PACS,>50,$YEAR,$TIME,$env:USERNAME";
                 
                 # In case the NIC goes down, delay here so highest resolution of outage is 5 seconds.
                 start-sleep -s 1
@@ -94,7 +94,7 @@ While($true)
                 # Log success to file.
                 $YEAR = get-date -format yyyy-MM-dd;
                 $TIME = get-date -format HH:mm:ss;
-                add-content C:\Users\gguaracha\Documents\Scripts\$logname "$hostname,$PACS,$pingtime,$YEAR,$TIME,$env:USERNAME";
+                add-content C:\Users\$USER\Documents\Scripts\$logname "$hostname,$PACS,$pingtime,$YEAR,$TIME,$env:USERNAME";
 
                 # Pause for 1 second to prevent low latency loop being too quick.
                 start-sleep -s 1
