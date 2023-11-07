818ee006d368b37d9fde28de07bdf38610b61af7
Gerardo <56288124+Dynecs@users.noreply.github.com>
2023-11-07 16:49:38 -0700

    Update Untitled2.ps1

--- Untitled2.ps1
+++ Untitled2.ps1
@@ -1,6 +1,6 @@
-﻿$Latency = Import-Csv C:\Users\gguaracha\Documents\Scripts\PingLog-$hostname.CSV | select -ExpandProperty Latency | Measure-Object -Average | select -ExpandProperty Average
+﻿$Latency = Import-Csv C:\Users\$USER\Documents\Scripts\PingLog-$hostname.CSV | select -ExpandProperty Latency | Measure-Object -Average | select -ExpandProperty Average
 
-if ( Test-Path C:\Users\gguaracha\Documents\Scripts\PingLog-$hostname.CSV -IsValid )
+if ( Test-Path C:\Users\$USER\Documents\Scripts\PingLog-$hostname.CSV -IsValid )
             {
-                Add-Content C:\Users\gguaracha\Documents\Scripts\PingLog-$hostname.CSV "Average Latency,,$Latency";
+                Add-Content C:\Users\$USER\Documents\Scripts\PingLog-$hostname.CSV "Average Latency,,$Latency";
             }
